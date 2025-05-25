d0af9415e7b186662676beb4eaa03799f27988cb
Kasatkin I.A <kasatkin.ia@edu.spbstu.ru>
2025-05-25 21:08:40 +0300

    Изменн вывод графики в окне победы

--- src/ui/Menu.c
+++ src/ui/Menu.c
@@ -92,9 +92,11 @@ void showMainMenu(Window* window)   //исправлены утечки
     TTF_CloseFont(font);
     FreeButtons(buttons, 5);
 
+    GameInfo info = {0, 0, 0};
+
     switch (selectedItem)
     {
-        case 0: LoseMenu(window); break;//SetDifficult(window); break;
+        case 0: WinMenu(window, info); break;//SetDifficult(window); break;
         case 1: LoadGame(window); break;
         case 2: ShowLeaderBoard(window); break;
         case 3: ShowAbout(window); break;
@@ -910,22 +912,21 @@ void WinMenu(Window* window, GameInfo info) //исправлены утечки
     SDL_Renderer* renderer = window->renderer;
 
     SDL_Color Black = {0, 0, 0, 255};
-    SDL_Color Green = {0, 255, 0, 255};
     SDL_Color red = {255, 0, 0, 255};
     SDL_Color Grey = {178, 178, 178, 255};
 
     // Загрузка шрифта с увеличенным размером
     TTF_Font* font1 = TTF_OpenFont("assets/fonts/minecraft.ttf", FONT_SIZE);
     TTF_Font* font2 = TTF_OpenFont("assets/fonts/freesansbold.ttf", 80);
-    TTF_Font* font3 = TTF_OpenFont("assets/fonts/bleedingcowboysrus.ttf", 120);
-    if (!font1 || !font3) 
+    TTF_Font* font3 = TTF_OpenFont("assets/fonts/minecraft.ttf", 100);
+    if (!font1 || !font2 || !font3) 
     {
         fprintf(stderr, "Failed to load font: %s", TTF_GetError());
         return;
     }
 
     // Пункты меню с увеличенными размерами и отступами
-    MenuItem items[3];
+    MenuItem* items = (MenuItem*)malloc(sizeof(MenuItem) * 2);
 
     SDL_Texture* button = Create_colored_rect(renderer, 400, 80, 255, 255, 255, 255);
     items[0].rect.x = SCREEN_WIDTH/2 - 250; items[0].rect.y = 400;
@@ -935,7 +936,26 @@ void WinMenu(Window* window, GameInfo info) //исправлены утечки
     items[1].rect.x = SCREEN_WIDTH/2 - 255; items[1].rect.y = 395;
     items[1].rect.w = 510;  items[1].rect.h = 90;
 
-    CreateTextButton(&items[2], font1, "Main menu", SCREEN_WIDTH/2 - 200, 600, 400, 80);
+    Button* buttons = (Button*)malloc(sizeof(Button));
+    CreateTextButton1(renderer, buttons, font1, "Main menu", SCREEN_WIDTH/2, 600);
+
+    //Создание надписи о победе
+    TTF_SetFontOutline(font3, 2);
+    SDL_Surface* outline_surface = TTF_RenderText_Blended(font3, "YOU WIIIIIIIIN!!!", Black);
+    TTF_SetFontOutline(font3, 0);
+    SDL_Surface* text_surface = TTF_RenderText_Blended(font3, "YOU WIIIIIIIIN!!!", red);
+    SDL_Surface* Your_name_surface = TTF_RenderText_Blended(font2, "Your name", Grey);
+
+    SDL_Texture* outline_texture = SDL_CreateTextureFromSurface(renderer, outline_surface);
+    SDL_Texture* text_texture = SDL_CreateTextureFromSurface(renderer, text_surface);
+    SDL_Texture* Your_name_texture = SDL_CreateTextureFromSurface(renderer, Your_name_surface);
+
+    SDL_Rect outline_rect = {SCREEN_WIDTH/2 - outline_surface->w / 2, 200, outline_surface->w, outline_surface->h};
+    SDL_Rect text_rect = {SCREEN_WIDTH/2 - text_surface->w / 2, 200, text_surface->w, text_surface->h};
+
+    SDL_FreeSurface(outline_surface);
+    SDL_FreeSurface(text_surface);
+    SDL_FreeSurface(Your_name_surface);
 
     bool running = true;
     int selectedItem = -1;
@@ -950,9 +970,17 @@ void WinMenu(Window* window, GameInfo info) //исправлены утечки
             switch (event.type) 
             {
             case SDL_QUIT:
+                free(items);
                 TTF_CloseFont(font1);
                 TTF_CloseFont(font2);
                 TTF_CloseFont(font3);
+                FreeButtons(buttons, 1);
+                SDL_DestroyTexture(button);
+                SDL_DestroyTexture(text_texture);
+                SDL_DestroyTexture(button_outline);
+                SDL_DestroyTexture(outline_texture);
+                SDL_DestroyTexture(Your_name_texture);
+                SDL_DestroyTexture(Your_name_texture);
                 Game_cleanup(window, EXIT_SUCCESS);
                 break;
 
@@ -960,19 +988,31 @@ void WinMenu(Window* window, GameInfo info) //исправлены утечки
                 int x = event.motion.x;
                 int y = event.motion.y;
 
-                for (int i = 0; i < 3; i++) 
+                for (int i = 0; i < 2; i++) 
                     items[i].hovered = (x >= items[i].rect.x && x <= items[i].rect.x + items[i].rect.w && 
                                         y >= items[i].rect.y && y <= items[i].rect.y + items[i].rect.h);
+                                        
+                buttons->hovered = (x >= buttons->out_rect.x && x <= buttons->out_rect.x + buttons->out_rect.w && 
+                                    y >= buttons->out_rect.y && y <= buttons->out_rect.y + buttons->out_rect.h);
                 break;
                 
             case SDL_MOUSEBUTTONDOWN:
-                if (event.button.button == SDL_BUTTON_LEFT) {
-                    for (int i = 0; i < 3; i++) {
-                        if (items[i].hovered) {
+                if (event.button.button == SDL_BUTTON_LEFT)
+                {
+                    for (int i = 0; i < 2; i++)
+                    {
+                        if (items[i].hovered)
+                        {
                             selectedItem = i;
                             running = false;
                             break;
                         }
+                    }
+                    if (buttons->hovered)
+                    {
+                        selectedItem = 2;
+                        running = false;
+                        break;
                     }
                 }
                 break;
@@ -983,14 +1023,14 @@ void WinMenu(Window* window, GameInfo info) //исправлены утечки
         SDL_RenderClear(renderer);
         SDL_RenderCopy(renderer, window->background, NULL, NULL);
 
-        SDL_Color color = items[2].hovered ? red : Green;
-        renderText(renderer, font1, items[2].text, items[2].rect.x, items[2].rect.y, color, &Black);
-        renderText(renderer, font3, "YOU WIIIIIIIIN!!!", SCREEN_WIDTH/2 - 400, 200, red, &Black);
-        
-        
-        SDL_RenderCopy(renderer, button_outline, NULL, &items[1].rect);
-        SDL_RenderCopy(renderer, button, NULL, &items[0].rect);
-        renderText(renderer, font2, "Your name", items[0].rect.x, items[0].rect.y, Grey, NULL);
+        renderButton(renderer, buttons);
+
+        SDL_RenderCopy(renderer, outline_texture, NULL, &outline_rect);
+        SDL_RenderCopy(renderer, text_texture, NULL, &text_rect);
+                
+        SDL_RenderCopy(renderer, button_outline,    NULL, &items[1].rect);
+        SDL_RenderCopy(renderer, button,            NULL, &items[0].rect);
+        SDL_RenderCopy(renderer, Your_name_texture, NULL, &items[0].rect);
 
         SDL_RenderPresent(renderer);
 
@@ -999,9 +1039,17 @@ void WinMenu(Window* window, GameInfo info) //исправлены утечки
         SDL_Delay(1000/FPS - frameTime);
     }
 
+    free(items);
     TTF_CloseFont(font1);
     TTF_CloseFont(font2);
     TTF_CloseFont(font3);
+    FreeButtons(buttons, 1);
+    SDL_DestroyTexture(button);
+    SDL_DestroyTexture(text_texture);
+    SDL_DestroyTexture(button_outline);
+    SDL_DestroyTexture(outline_texture);
+    SDL_DestroyTexture(Your_name_texture);
+    SDL_DestroyTexture(Your_name_texture);
 
     switch (selectedItem)
     {
