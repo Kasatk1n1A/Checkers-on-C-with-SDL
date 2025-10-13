f5c0e1fb4c4f67f0c674b0682f8116668e6f17c3
Kasatk1n1A <kasatkin.ia@edu.spbstu.ru>
2025-10-13 05:01:14 +0300

    added possibility exit from game, while bot thinking

--- src/core/board.c
+++ src/core/board.c
@@ -153,6 +153,16 @@ void out_board_SDL(Window* window, Board* CheckersBoard)
 
 void renderBoardFrame(Window* window, Board* CheckersBoard)
 {    
+    SDL_Event event;
+    while (SDL_PollEvent(&event)) 
+    {
+        switch (event.type) 
+        {
+        case SDL_QUIT:
+            exit(0);
+            break;
+        }
+    }
     // Очистка экрана
     SDL_RenderClear(window->renderer);
         
