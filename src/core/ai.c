e87d3e002a80b0c1fdf433cca0c1c56e3da9d9af
Kasatk1n1A <kasatkin.ia@edu.spbstu.ru>
2025-10-13 03:26:38 +0300

    In LRU and MRU have been fixed find functions, now when element found it`s push back into list

--- src/core/ai.c
+++ src/core/ai.c
@@ -409,8 +409,8 @@ int minimax(CH_Type** board, CH_Type** tmp_board, int depth, int alpha, int beta
     Transposition* trans = Transposition_Create(tmp_board, curr_player, curr_eval, depth, NULL);
 
     // Transposition* tmp = (Transposition*)HashTable_Find(hash_table, trans);
-    //Transposition* tmp = (Transposition*)LRUCache_Find(LRU_cache, (void*)trans);
-    Transposition* tmp = (Transposition*)MRUCache_Find(MRU_cache, (void*)trans);
+    Transposition* tmp = (Transposition*)LRUCache_Find(LRU_cache, (void*)trans);
+    // Transposition* tmp = (Transposition*)MRUCache_Find(MRU_cache, (void*)trans);
 
     if (tmp && tmp->depth <= trans->depth){
         Transposition_Delete(&trans);
@@ -467,8 +467,8 @@ int minimax(CH_Type** board, CH_Type** tmp_board, int depth, int alpha, int beta
     }
     else{
         // HashTable_Add(hash_table, trans);
-        // LRUCache_add(LRU_cache, trans);
-        MRUCache_add(MRU_cache, trans);
+        LRUCache_add(LRU_cache, trans);
+        // MRUCache_add(MRU_cache, trans);
     }
     free_move(moves);
     return best_eval;
@@ -525,8 +525,8 @@ Move* find_best_move(CH_Type** board, Player player, int maxDepth)
 void bot_make_move(CH_Type** board, int difficult, Player player)
 {
     // hash_table = HashTable_create(Transposition_Compare, Transposition_hash, _Transposition_Delete_);
-    // LRU_cache = LRUCache_create(Transposition_Compare, Transposition_hash, _Transposition_Delete_);
-    MRU_cache = MRUCache_create(Transposition_Compare, Transposition_hash, _Transposition_Delete_);
+    LRU_cache = LRUCache_create(Transposition_Compare, Transposition_hash, _Transposition_Delete_);
+    // MRU_cache = MRUCache_create(Transposition_Compare, Transposition_hash, _Transposition_Delete_);
 
     // Установка глубины поиска в зависимости от сложности
     int maxDepth = difficult;
@@ -565,8 +565,8 @@ void bot_make_move(CH_Type** board, int difficult, Player player)
 
     // printf("size: %d\n", hash_table->size);
 
-    MRUCache_delete(MRU_cache);
-    // LRUCache_delete(LRU_cache);
+    // MRUCache_delete(MRU_cache);
+    LRUCache_delete(LRU_cache);
     // HashTable_clean(hash_table);
     // HashTable_delete(hash_table);
 }
